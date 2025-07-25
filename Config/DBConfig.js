2edf5de14b5f7b7794a3f5c0c5a3f7eea73d0031
Pratik <pratiknpawar4912@gmail.com>
2025-07-25 21:27:03 +0530

    blooms backend

--- Config/DBConfig.js
+++ Config/DBConfig.js
@@ -1,29 +1,15 @@
-// require("dotenv").config();
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   host:'localhost'|| process.env.DB_HOST,
-//   port: 5432,
-//   user:'postgres'|| process.env.DB_USER,
-//   password:'4912'|| process.env.DB_PASSWORD,
-//   database:'bloomsbackend'|| process.env.DB_NAME,
-//   ssl: {
-//     rejectUnauthorized: false, // required for Render
-//   },
-// });
-
-// module.exports = pool;
-
 require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: "localhost" || process.env.DB_HOST,
+  host:process.env.DB_HOST,
   port: 5432,
-  user: "postgres" || process.env.DB_USER,
-  password: "4912" || process.env.DB_PASSWORD,
-  database: "bloomsbackend" || process.env.DB_NAME,
-  ssl: false
+  user:process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false, // required for Render
+  },
 });
 
 module.exports = pool;
